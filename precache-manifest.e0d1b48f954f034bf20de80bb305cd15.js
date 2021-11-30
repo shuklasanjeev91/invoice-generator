7d09cc8126dd46204a8cb8bc3ac97290256d5144
Sanjeev Shukla <sanjeevshukla@Sanjeevs-MacBook-Air.local>
2021-11-30 14:25:21 +0530

    Updates

---
+++ precache-manifest.e0d1b48f954f034bf20de80bb305cd15.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef3cc6c0045ccec711de581d092b8af",
+    "url": "/invoice-generator/index.html"
+  },
+  {
+    "revision": "55bb6a0a7f7a68992ed2",
+    "url": "/invoice-generator/static/css/4.429e79c4.chunk.css"
+  },
+  {
+    "revision": "4aed7daa6aa31205bf6b",
+    "url": "/invoice-generator/static/css/5.4c71d463.chunk.css"
+  },
+  {
+    "revision": "d53e2950a5756cd32c15",
+    "url": "/invoice-generator/static/css/main.c321e22f.chunk.css"
+  },
+  {
+    "revision": "835aeed6859352948d85",
+    "url": "/invoice-generator/static/js/0.6cd1a969.chunk.js"
+  },
+  {
+    "revision": "eae3e86dfd531f755769266e9e336c9b",
+    "url": "/invoice-generator/static/js/0.6cd1a969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995e38c3d32e8cfcdd3a",
+    "url": "/invoice-generator/static/js/3.13303816.chunk.js"
+  },
+  {
+    "revision": "55bb6a0a7f7a68992ed2",
+    "url": "/invoice-generator/static/js/4.773aef2e.chunk.js"
+  },
+  {
+    "revision": "51b545c0560917d603d1236a44aad71f",
+    "url": "/invoice-generator/static/js/4.773aef2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aed7daa6aa31205bf6b",
+    "url": "/invoice-generator/static/js/5.221f3e67.chunk.js"
+  },
+  {
+    "revision": "1598d5a0932498e45165d29717a4d579",
+    "url": "/invoice-generator/static/js/5.221f3e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fddc15c51be02d44464e",
+    "url": "/invoice-generator/static/js/6.63d3ef6d.chunk.js"
+  },
+  {
+    "revision": "910d91bd04ebdbbd382f",
+    "url": "/invoice-generator/static/js/7.5f907cd9.chunk.js"
+  },
+  {
+    "revision": "3736d33bee399d86aa22",
+    "url": "/invoice-generator/static/js/8.59e189bf.chunk.js"
+  },
+  {
+    "revision": "d53e2950a5756cd32c15",
+    "url": "/invoice-generator/static/js/main.fe0075b7.chunk.js"
+  },
+  {
+    "revision": "fa36a2dfeacc2c2cc8e2",
+    "url": "/invoice-generator/static/js/runtime-main.2ef36756.js"
+  },
+  {
+    "revision": "ee84c0a5ca7949dbff1bdb4b885f74b8",
+    "url": "/invoice-generator/static/media/Template 1.ee84c0a5.jpg"
+  },
+  {
+    "revision": "8b32287c57f44eea096355df72c75be8",
+    "url": "/invoice-generator/static/media/Template 2.8b32287c.jpg"
+  },
+  {
+    "revision": "26819f93127cb5cc33b27b7766535475",
+    "url": "/invoice-generator/static/media/Template 3.26819f93.jpg"
+  },
+  {
+    "revision": "617e42666152531d43d66f34c85e9da5",
+    "url": "/invoice-generator/static/media/Template 4.617e4266.jpg"
+  },
+  {
+    "revision": "5ae6fa860179cda1f1374e8e2a326f25",
+    "url": "/invoice-generator/static/media/Template 5.5ae6fa86.jpg"
+  }
+]);
